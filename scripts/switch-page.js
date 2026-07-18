039b6d1ff1b52219e9935e3fa0d58e8b35d97d73
Harsh <harsh1111kashyap@gmail.com>
2026-07-18 21:40:12 +0530

    add saving page

--- scripts/switch-page.js
+++ scripts/switch-page.js
@@ -2,21 +2,35 @@
 let dashboardPage = document.getElementById("dashboard-section");
 let expansePage = document.getElementById("expanse-list-section");
 let aboutPage = document.getElementById("about-page-section");
+let savingPage = document.getElementById("saving-page-section");
 
 function dashboard(){
     dashboardPage.style.display = "flex";
     expansePage.style.display = "none";
     aboutPage.style.display = "none";
+    savingPage.style.display = "none";
+
 }
 
 function about(){
     aboutPage.style.display = "flex";
     dashboardPage.style.display = "none";
     expansePage.style.display = "none";
+    savingPage.style.display = "none";
+
 }
 
 function transaction(){
     expansePage.style.display = "flex";
+    dashboardPage.style.display = "none";
+    aboutPage.style.display = "none";
+    savingPage.style.display = "none";
+
+}
+
+function saving(){
+    savingPage.style.display = "flex";
+    expansePage.style.display = "none";
     dashboardPage.style.display = "none";
     aboutPage.style.display = "none";
 }
